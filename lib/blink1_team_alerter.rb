e36e9c2a2e1f6898cc3ff67599635886e5547990
Christian Lövsund <chris@lovsund.se>
2015-05-04 21:40:12 +0200

    cleanup code

--- lib/blink1_team_alerter.rb
+++ lib/blink1_team_alerter.rb
@@ -2,12 +2,10 @@ require "blink1_team_alerter/version"
 require 'rest-client'
 require 'json'
 require 'blink1'
-#require 'blink1-patterns' 
 
 module Blink1TeamAlerter
 
-#  check_jira
-  
+  #  check_jira
   def self.check_jira(username, password, host, project_key)
     jira_url = "https://#{username}:#{password}@#{host}/rest/api/2/search?"
     # latest 5 issues from a project with '0' priority
@@ -20,38 +18,37 @@ module Blink1TeamAlerter
     end
 
     data = JSON.parse(response.body)
-     if data['issues'].any?
-       data['issues'].each do |issue|
-         puts "Key: #{issue['key']}, Summary: #{issue['fields']['summary']}"
-       end 
-         #      Blink::Patterns.police
-       Blink1.open do |blink1|
-#      blink1.write_pattern_line(100, 255, 255, 255, 0)
-#      blink1.write_pattern_line(100, 0,   255, 255, 1)
-#      blink1.write_pattern_line(100, 255, 255, 0,   2)
-#      blink1.write_pattern_line(100, 255, 0,   255, 3)
-#      blink1.write_pattern_line(100, 255, 255, 255, 4)
-#      blink1.write_pattern_line(100, 0,   255, 255, 5)
-#      blink1.write_pattern_line(100, 255, 255, 0,   6)
-#      blink1.write_pattern_line(100, 255, 0,   255, 7)
-#      blink1.write_pattern_line(100, 255, 255, 255, 8)
-#      blink1.write_pattern_line(100, 0,   255, 255, 9)
-#      blink1.write_pattern_line(100, 255, 255, 0,   10)
-#      blink1.play(0)
-       blink1.set_rgb(255, 0, 0)
-     end
+    if data['issues'].any?
+      data['issues'].each do |issue|
+        puts "Key: #{issue['key']}, Summary: #{issue['fields']['summary']}"
+      end
+      Blink1.open do |blink1|
+        #      blink1.write_pattern_line(100, 255, 255, 255, 0)
+        #      blink1.write_pattern_line(100, 0,   255, 255, 1)
+        #      blink1.write_pattern_line(100, 255, 255, 0,   2)
+        #      blink1.write_pattern_line(100, 255, 0,   255, 3)
+        #      blink1.write_pattern_line(100, 255, 255, 255, 4)
+        #      blink1.write_pattern_line(100, 0,   255, 255, 5)
+        #      blink1.write_pattern_line(100, 255, 255, 0,   6)
+        #      blink1.write_pattern_line(100, 255, 0,   255, 7)
+        #      blink1.write_pattern_line(100, 255, 255, 255, 8)
+        #      blink1.write_pattern_line(100, 0,   255, 255, 9)
+        #      blink1.write_pattern_line(100, 255, 255, 0,   10)
+        #      blink1.play(0)
+        blink1.set_rgb(255, 0, 0)
+        puts "Blink1 Red"
+      end
     else
-#      Blink::Patterns.off      
       Blink1.open do |blink1|
         blink1.set_rgb(0, 255, 0)
-     end
-     end
-   end
+      end
+      puts "Blink1 Green"
+    end
+  end
 
-  def blink1_off 
+  def blink1_off
     Blink1.open do |blink1|
       blink1.off
     end
-  end  
+  end
 end
-
