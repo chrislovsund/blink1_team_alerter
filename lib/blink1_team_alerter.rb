f0221327f9fa007a4214e96463204f7e3133787e
Christian Lövsund <christian.lovsund@jeppesen.com>
2015-11-13 11:26:32 +0100

    create html status page with same color as blink1, to be used for various reasons.

--- lib/blink1_team_alerter.rb
+++ lib/blink1_team_alerter.rb
@@ -1,19 +1,22 @@
-require "blink1_team_alerter/version"
+require 'blink1_team_alerter/version'
 require 'blink1'
 require 'gocd_client'
 require 'jira_client'
 
 module Blink1TeamAlerter
   def self.check_for_alerts(jira, alert_filter, warn_filter, gocd)
-
     if jira.has_new_prio_0_issues? alert_filter
       blink1_police
+      create_html_status_page('red')
     elsif jira.has_ongoing_prio_0_issues? warn_filter
       blink1_yellow
+      create_html_status_page('yellow')
     elsif gocd.has_failing_projects?
       blink1_purple
+      create_html_status_page('purple')
     else
       blink1_blue
+      create_html_status_page('blue')
     end
   end
 
@@ -24,7 +27,7 @@ module Blink1TeamAlerter
   end
 
   def self.blink1_police
-    puts "Blink1 Police"
+    puts 'Blink1 Police'
     30.times do
       system( "blink1-tool -l 2 --red " )
       system( "blink1-tool -l 1 --blue" )
@@ -33,6 +36,12 @@ module Blink1TeamAlerter
       system( "blink1-tool -l 2 --blue" )
       system( "sleep 0.5" )
     end
+  end
+
+  def create_html_status_page(color)
+    status_file = File.new('status.html', 'w+')
+    status_file.puts "<HTML><BODY BGCOLOR='#{color}'></BODY></HTML>"
+    status_file.close
   end
 
   def self.blink1_red
