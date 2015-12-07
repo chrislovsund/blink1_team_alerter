27a1e3f4f9e81803c6f46c19ae40c10ebe786cbb
Christian Lövsund <christian.lovsund@jeppesen.com>
2015-12-07 08:58:27 +0100

    Improve status report for GoCD issues

--- lib/gocd_client.rb
+++ lib/gocd_client.rb
@@ -27,7 +27,7 @@ class GocdClient
         next unless stage['result'] == 'Failed'
         stage_url = "#{@gocd_addr}/go/pipelines/#{pipe_name}/#{event['counter']}/#{stage['name']}/#{stage['counter']}"
         stage_display_name = "#{pipe_name} - #{event['label']} Stage #{stage['name']} = #{stage['result']}"
-        status_html += "<a href=\"#{stage_url}\" target=\"_blank\">#{stage_display_name}</a>\n"
+        status_html += "<a href=\"#{stage_url}\" target=\"_blank\" style=\"color:white\" >#{stage_display_name}</a><br>\n"
       end
     end
     status_html
